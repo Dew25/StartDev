6f4257b833235207d95713418e9f1b9dab80c804
none <none@ee.ee>
2020-09-23 16:37:56 +0300

    Добавлена 7-я задача на строки

---
+++ src/startdev/Task7.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package startdev;
+
+/**
+ *
+ * @author user
+ */
+public class Task7 {
+    public void run(){
+        System.out.println("----- Задача 7 ------");
+        System.out.println("Введите строку");
+        // посчитайте сколько раз в строке встречается введенная вами буква
+        System.out.println("Введите букву: ");
+        String letter = ?;
+        int n = 0;
+        
+        System.out.println("Буква \""+letter+"\" встречается в тексте "+ n + "раз");
+        System.out.println("");
+        System.out.println("----- конец задачи 6 ------");
+    }
+}
